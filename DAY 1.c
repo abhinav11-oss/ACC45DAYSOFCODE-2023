a5139938dc1ce81e457031e9cd938c185db1326a
abhinav11-oss <144876337+abhinav11-oss@users.noreply.github.com>
2023-09-18 19:58:16 +0530

    Add files via upload
    day 1 Abhinav Lodhi Section A Btech CSE

---
+++ DAY 1.c
@@ -0,0 +1,14 @@
+#day 1 
+#Abhinav Lodhi Section A
+int main(){
+    int X;
+    printf("How much amount of water you drank today- ");
+    scanf("%d",&X);
+    if(X>=2000){
+        printf("YES,You are following advise of doctor Well done");
+    }else{
+        printf("No,You are not following advise of doctor");
+    }
+   
+}
+
